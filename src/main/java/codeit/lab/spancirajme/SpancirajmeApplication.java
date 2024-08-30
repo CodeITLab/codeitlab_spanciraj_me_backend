ccd7008fe929734b6f874177ff057b76460223dd
kristijanfistrek <fistrekkristijan@gmail.com>
2024-08-30 22:06:30 +0200

    Add: initial commit

---
+++ src/main/java/codeit/lab/spancirajme/SpancirajmeApplication.java
@@ -0,0 +1,13 @@
+package codeit.lab.spancirajme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpancirajmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpancirajmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeit/lab/spancirajme/SpancirajmeApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.lab.spancirajme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpancirajmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
